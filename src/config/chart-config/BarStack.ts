fd6c448c054764090dda66d4c2ff3b3091392421
冯尧 <fengyao@glaway.com>
2020-04-21 16:06:39 +0800

    重构图表配置文件结构

---
+++ src/config/chart-config/BarStack.ts
@@ -0,0 +1,47 @@
+import { BarConfig, BarChartOption } from "./Bar";
+import ObjectUtil from "@/util/ObjectUtil";
+import { ChartType } from "@/enums/ChartType";
+import { ChartConfigItem } from "../ChartConfig";
+
+/**
+ * 图表类型
+ */
+export type BarStackChartOption = BarChartOption;
+
+/**
+ * 初始化模板
+ */
+const templates = {
+  echarts: {
+    sampleStyle: {
+      barStack: ObjectUtil.copy(BarConfig.templates.echarts.sampleStyle.bar)
+    }
+  }
+};
+
+/**
+ * 菜单选项
+ */
+const menuOptions = ObjectUtil.copy(BarConfig.menuOptions);
+
+/**
+ * 创建菜单配置
+ */
+const createMenuConfig = {
+  iconClass: "gw-icon gw-iconfsux_tubiao_duijizhuzhuangtu",
+  title: "堆积柱图",
+  createType: ChartType.barStack,
+  enable: true
+};
+
+/**
+ * 配置项
+ */
+const config = ObjectUtil.copy(BarConfig.config);
+
+export const BarStackConfig: ChartConfigItem = {
+  templates,
+  menuOptions,
+  createMenuConfig,
+  config
+};
