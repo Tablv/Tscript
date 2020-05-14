9618b71b97da1a3ce25d0dced63be73227fd3480
TabLv <1204479385@qq.comm>
2020-05-14 18:02:10 +0800

    修改线形图继承ChartHandler接口

--- src/service/chart-handler/LineHandler.ts
+++ src/service/chart-handler/LineHandler.ts
@@ -1,10 +1,69 @@
-import BarHandler from "./BarHandler";
 import EChartDataUtil from "@/util/EChartDataUtil";
+import { AnalysisResults } from "@/model/types/AnalysisResults";
+import { SplitedFieldNames } from "../EChartsService";
+import Dashboard from "@/model/view/dashboard/Dashboard";
+import EChartsService from "../EChartsService";
+import { ChartHandler } from "../interfaces/ChartHandler";
+import { LineChartOption } from "@/config/ChartOption";
+import ObjectUtil from "@/util/ObjectUtil";
+import warnConfigure from "./configure/WarnConfigure";
+import { WARN_DEFAULT_VALUE } from "@/model/view/Warn";
 
 /**
  * 折线图处理
  */
-export default class LineHandler extends BarHandler {
+export default class LineHandler implements ChartHandler {
+  /**
+   * 分析字段
+   */
+  public fieldNames: SplitedFieldNames;
+
+  /**
+   * 数据设置
+   * 后面有其他的设置也加入到这里
+   * @param result 分析结果
+   * @param dashboard 仪表盘数据
+   * @param sampleStyle 样例样式
+   */
+  constructor(
+    public result: AnalysisResults,
+    public dashboard: Dashboard,
+    public sampleStyle: LineChartOption
+  ) {
+    this.fieldNames = EChartsService.splitFieldNames(
+      this.result[0],
+      this.dashboard
+    );
+  }
+
+  /**
+   * 获取图表的处理结果
+   */
+  public getStyle(): echarts.EChartOption {
+    let style: echarts.EChartOption = {};
+
+    if (ObjectUtil.isEmpty(this.result)) {
+      style.xAxis = [];
+      style.yAxis = [];
+      style.series = [];
+      return {};
+    }
+
+    style.xAxis = this.getXAxis();
+    style.yAxis = this.getYAxis();
+    style.series = this.getSeries();
+    style.legend = this.getLegend();
+
+    /**
+     * 预警处理
+     */
+    if (this.dashboard.analysis.warn.id !== WARN_DEFAULT_VALUE) {
+      warnConfigure(this.dashboard, style);
+    }
+
+    return style;
+  }
+
   /**
    * 获取X轴数据
    */
@@ -22,6 +81,18 @@ export default class LineHandler extends BarHandler {
     });
 
     return xAxis;
+  }
+
+  /**
+   * 获取Y轴数据
+   */
+  public getYAxis(): Array<echarts.EChartOption.YAxis> {
+    return [
+      {
+        type: "value",
+        name: this.sampleStyle.decimals.unit
+      }
+    ] as Array<echarts.EChartOption.YAxis>;
   }
 
   /**
@@ -47,5 +118,15 @@ export default class LineHandler extends BarHandler {
     });
 
     return series;
+  }
+
+  /**
+   * 获取图例
+   */
+  public getLegend(): echarts.EChartOption.Legend {
+    const legend = {
+      data: this.fieldNames.measures
+    };
+    return legend;
   }
 }
