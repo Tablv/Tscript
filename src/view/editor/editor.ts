5cbc8bd0823e2e883af0cbc433de52794d57e7c2
冯尧 <fengyao@glaway.com>
2020-05-20 15:27:19 +0800

    添加 bi-component

--- src/view/editor/editor.ts
+++ src/view/editor/editor.ts
@@ -12,6 +12,9 @@ import "@/dependencies/element-ui.js";
 // Vue-Clipboard
 import "@/dependencies/vue-clipboard";
 
+// BI 依赖
+import "@/dependencies/glaway-bi"
+
 // Svg Icons
 import "@/assets/font/svg_icon.css";
 import "@/assets/font/svg_icon.js";
